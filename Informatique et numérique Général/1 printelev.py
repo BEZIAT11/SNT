91e661668f2a3c050d0930c0c047aafc2ec83d37
atrehet <54683910+atrehet@users.noreply.github.com>
2019-09-11 22:51:39 +0200

    Add files via upload

---
+++ Informatique et numérique Général/1 printelev.py
@@ -0,0 +1,36 @@
+print ("Exo 1")
+# Exo 1 : Lance le programme, puis indique  à quoi sert la FONTION print( )?
+print(5)
+print()
+print("bonjour")
+print("bonjour" * 5)
+print("bonjour" + "bonjour")
+print("Comment cela va ?")
+
+print ("Exo 2")
+# Exo 2 : Sur votre cahier, faire 2 colonnes. Ecrire ce qui suit
+# à gauche et le résultat obtenu à droite
+# Si le résultat est Error, tester une correction et
+# la noter si elle fonctionne
+print(5+8)
+print("5+8")
+Print(5)
+print5
+print(bonjour)
+
+print ("Exo 3")
+# Exo 3 : Ecrire, sous chaque tiret "_", la ligne de code permettant de
+# voir s'afficher dans l'interpréteur :
+# - le résultat de 5*3
+# - le texte : 5*3
+# - le résultat de 5,5*10
+# - le résultat de 10/3
+# Sur votre cahier, faire 2 colonnes. Ecrire ce que vous avez programmé
+# à gauche et le résultat obtenu à droite (en corrigeant ou expliquant
+# les erreurs si nécessaire)
+
+print ("Exo 4")
+# Exo 4 : Ecrire la ligne de code permettant de
+# voir s'afficher dans l'interpréteur : "Bonjour, je suis un programme
+# doué en calcul. Je sais que 1000 * 6,5 + 500 + 800 - 23
+# donne " suivi du résultat.
---
+++ Informatique et numérique Général/3 inputelev.py
@@ -0,0 +1,18 @@
+# Exo1 Lis ce programme puis lance le en répondant aux questions posées.
+print(" Exo 1")
+# écrire une question
+print("Quel est ton prénom ?")
+# récupérer le "texte" de la réponse
+# et le placer dans la variable "prenom"
+prenom = input()
+# écrire une phrase avec prenom
+print("Bonjour", prenom)
+
+# Exo2
+# a- Recopie le programme ci-dessus et colle-le sous print(Exo 2)
+# b- Crée un programme pour
+# - demander et récupérer la date de naissance de l'utilisateur du programme dans la variable "Naissance"
+# - écrire une phrase avec prenom et naissance
+# - ajouter des explication précédées de #
+print()
+print(" Exo 2")
